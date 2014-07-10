6b74c37b4ec9586cec556e75d5d3854ebf5b66db
Yoshikuni Jujo <PAF01143@nifty.ne.jp>
2014-07-10 10:10:27 +0900

    check my remote server by my client

---
+++ examples/checkMyServer.hs
@@ -0,0 +1,35 @@
+{-# LANGUAGE OverloadedStrings, PackageImports #-}
+
+import Control.Applicative
+import Control.Monad
+import "monads-tf" Control.Monad.Trans
+import Data.HandleLike
+import System.Environment
+import Network
+import Network.PeyoTLS.ReadFile
+import Network.PeyoTLS.Client
+import "crypto-random" Crypto.Random
+
+import qualified Data.ByteString.Char8 as BSC
+
+main :: IO ()
+main = do
+	d : _ <- getArgs
+	ca <- readCertificateStore [
+		d ++ "/cacert.pem",
+		d ++ "/geotrust_global_ca.pem" ]
+--	h <- connectTo "localhost" $ PortNumber 443
+	h <- connectTo "skami3.iocikun.jp" $ PortNumber 443
+	g <- cprgCreate <$> createEntropyPool :: IO SystemRNG
+	(`run` g) $ do
+		p <- open h ["TLS_RSA_WITH_AES_128_CBC_SHA"] [] ca
+		unless ("skami3.iocikun.jp" `elem` names p) . error $
+			"certificate name mismatch: " ++ show (names p)
+		hlPut p "GET / HTTP/1.1 \r\n"
+		hlPut p "Host: localhost\r\n\r\n"
+		doUntil BSC.null (hlGetLine p) >>= liftIO . mapM_ BSC.putStrLn
+		hlClose p
+
+doUntil :: Monad m => (a -> Bool) -> m a -> m [a]
+doUntil p rd = rd >>= \x ->
+	(if p x then return . (: []) else (`liftM` doUntil p rd) . (:)) x
