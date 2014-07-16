5805b3d5b2416aa35e0b57862f880fb8e8f232ed
Yoshikuni Jujo <PAF01143@nifty.ne.jp>
2014-07-16 10:35:59 +0900

    #95 refactoring Extension: finished

--- src/Network/PeyoTLS/Codec/Extension.hs
+++ src/Network/PeyoTLS/Codec/Extension.hs
@@ -15,95 +15,90 @@ import qualified Crypto.Types.PubKey.ECC as ECC
 
 import Network.PeyoTLS.Codec.HSAlg(HashAlg(..), SignAlg(..))
 
+modNm :: String
+modNm = "Network.PeyoTLS.Codec.Extension"
+
 data Extension
-	= ESName [ServerName]
-	| EECurve [ECC.CurveName] | EEcPFrmt [EcPointFormat]
-	| ESsnTicketTls BS.ByteString
-	| ENextProtoNego BS.ByteString
-	| ERnInfo BS.ByteString
-	| ERaw EType BS.ByteString
+	= ESName [SName]
+	| EECrv [ECC.CurveName]     | EEPFrmt [EPFrmt]
+	| ESsnTcktTls BS.ByteString | ENxPrtNego BS.ByteString
+	| ERnInfo BS.ByteString     | ERaw EType BS.ByteString
 	deriving (Show, Eq)
 
-instance B.Bytable Extension where encode = encodeE; decode = B.evalBytableM B.parse
-instance B.Parsable Extension where parse = parseE
+instance B.Bytable Extension where
+	encode (ESName n) = B.encode . ERaw TSName . B.addLen w16 $ cmap B.encode n
+	encode (EECrv c) = B.encode . ERaw TECrv . B.addLen w16 $ cmap B.encode c
+	encode (EEPFrmt f) = B.encode . ERaw TEPFrmt . B.addLen w8 $ cmap B.encode f
+	encode (ESsnTcktTls t) = B.encode $ ERaw TSsnTcktTls t
+	encode (ENxPrtNego n) = B.encode $ ERaw TNxPrtNego n
+	encode (ERnInfo i) = B.encode . ERaw TRnInfo $ B.addLen w8 i
+	encode (ERaw t e) = B.encode t `BS.append` B.addLen w16 e
+	decode = B.evalBytableM B.parse
 
-encodeE :: Extension -> BS.ByteString
-encodeE (ESName sn) = encodeE . ERaw TSName . B.addLen w16 $ cmap B.encode sn
-encodeE (EECurve ec) = encodeE . ERaw TECurve . B.addLen w16 $ cmap B.encode ec
-encodeE (EEcPFrmt pf) = encodeE . ERaw TEcPFrt . B.addLen w8 $ cmap B.encode pf
-encodeE (ESsnTicketTls stt) = encodeE $ ERaw TSsnTicketTls stt
-encodeE (ENextProtoNego npn) = encodeE $ ERaw TNextProtoNego npn
-encodeE (ERnInfo ri) = encodeE . ERaw TRnInfo $ B.addLen w8 ri
-encodeE (ERaw et body) = B.encode et `BS.append` B.addLen w16 body
-
-parseE :: B.BytableM Extension
-parseE = do
-	(t, l) <- (,) <$> B.take 2 <*> B.take 2
-	case t of
+instance B.Parsable Extension where
+	parse = (,) <$> B.take 2 <*> B.take 2 >>= \(t, l) -> case t of
 		TSName -> ESName <$> (flip B.list B.parse =<< B.take 2)
-		TECurve -> EECurve <$> (flip B.list (B.take 2) =<< B.take 2)
-		TEcPFrt -> EEcPFrmt <$> (flip B.list (B.take 1) =<< B.take 1)
-		TSsnTicketTls -> ESsnTicketTls <$> B.take l
-		TNextProtoNego -> ENextProtoNego <$> B.take l
+		TECrv -> EECrv <$> (flip B.list (B.take 2) =<< B.take 2)
+		TEPFrmt -> EEPFrmt <$> (flip B.list (B.take 1) =<< B.take 1)
+		TSsnTcktTls -> ESsnTcktTls <$> B.take l
+		TNxPrtNego -> ENxPrtNego <$> B.take l
 		TRnInfo -> ERnInfo <$> (B.take =<< B.take 1)
 		_ -> ERaw t <$> B.take l
 
+cmap :: (a -> BS.ByteString) -> [a] -> BS.ByteString
+cmap = (BS.concat .) . map
+
 data EType
-	= TSName         | TECurve     | TEcPFrt     | TSsnTicketTls
-	| TNextProtoNego | TRnInfo | TRaw Word16 deriving (Show, Eq)
+	= TSName     | TECrv   | TEPFrmt | TSsnTcktTls
+	| TNxPrtNego | TRnInfo | TRaw Word16 deriving (Show, Eq)
 
 instance B.Bytable EType where
 	encode TSName = B.encode (0 :: Word16)
-	encode TECurve = B.encode (10 :: Word16)
-	encode TEcPFrt = B.encode (11 :: Word16)
-	encode TSsnTicketTls = B.encode (35 :: Word16)
-	encode TNextProtoNego = B.encode (13172 :: Word16)
+	encode TECrv = B.encode (10 :: Word16)
+	encode TEPFrmt = B.encode (11 :: Word16)
+	encode TSsnTcktTls = B.encode (35 :: Word16)
+	encode TNxPrtNego = B.encode (13172 :: Word16)
 	encode TRnInfo = B.encode (65281 :: Word16)
 	encode (TRaw et) = B.encode et
 	decode bs = case BS.unpack bs of
 		[w1, w2] -> Right $
 			case fromIntegral w1 `shiftL` 8 .|. fromIntegral w2 of
 				0 -> TSName
-				10 -> TECurve
-				11 -> TEcPFrt
-				35 -> TSsnTicketTls
-				13172 -> TNextProtoNego
+				10 -> TECrv
+				11 -> TEPFrmt
+				35 -> TSsnTcktTls
+				13172 -> TNxPrtNego
 				65281 -> TRnInfo
 				et -> TRaw et
-		_ -> Left "Extension: EType.decode"
+		_ -> Left $ modNm ++ ": EType.decode"
 
-data ServerName = SNHostName BS.ByteString | SNRaw NameType BS.ByteString
-	deriving (Show, Eq)
+data SName = SNHName BS.ByteString | SNRaw NType BS.ByteString deriving (Show, Eq)
 
-instance B.Bytable ServerName where
-	encode (SNHostName nm) = B.encode $ SNRaw NTHostName nm
+instance B.Bytable SName where
+	encode (SNHName nm) = B.encode $ SNRaw NTHName nm
 	encode (SNRaw nt nm) = B.encode nt `BS.append` B.addLen w16 nm
 	decode = B.evalBytableM B.parse
 
-instance B.Parsable ServerName where
-	parse = do
-		(t, n) <- (,) <$> B.take 1 <*> (B.take =<< B.take 2)
-		return $ case t of
-			NTHostName -> SNHostName n; _ -> SNRaw t n
+instance B.Parsable SName where
+	parse = (\t n -> case t of NTHName -> SNHName n; _ -> SNRaw t n)
+		<$> B.take 1 <*> (B.take =<< B.take 2)
 
-data NameType = NTHostName | NTRaw Word8 deriving (Show, Eq)
+data NType = NTHName | NTRaw Word8 deriving (Show, Eq)
 
-instance B.Bytable NameType where
-	encode NTHostName = BS.pack [0]
+instance B.Bytable NType where
+	encode NTHName = BS.pack [0]
 	encode (NTRaw t) = BS.pack [t]
 	decode bs = case BS.unpack bs of
-		[t] -> Right $ case t of 0 -> NTHostName; _ -> NTRaw t
-		_ -> Left "Extension: NameType.decode"
+		[t] -> Right $ case t of 0 -> NTHName; _ -> NTRaw t
+		_ -> Left $ modNm ++ ": NType.decode"
 
 instance B.Bytable DH.Params where
-	encode (DH.Params p g) =
-		BS.concat [B.addLen w16 $ B.encode p, B.addLen w16 $ B.encode g]
+	encode (DH.Params p g) = BS.concat
+		[B.addLen w16 $ B.encode p, B.addLen w16 $ B.encode g]
 	decode = B.evalBytableM B.parse
 
 instance B.Parsable DH.Params where
-	parse = DH.Params
-		<$> (B.take =<< B.take 2)
-		<*> (B.take =<< B.take 2)
+	parse = DH.Params <$> (B.take =<< B.take 2) <*> (B.take =<< B.take 2)
 
 instance B.Bytable DH.PublicNumber where
 	encode = B.addLen w16 . B.encode . \(DH.PublicNumber pn) -> pn
@@ -112,24 +107,18 @@ instance B.Bytable DH.PublicNumber where
 instance B.Parsable DH.PublicNumber where
 	parse = fromInteger <$> (B.take =<< B.take 2)
 
-data EcCurveType = ExplicitPrime | ExplicitChar2 | NamedCurve | ECTRaw Word8
-	deriving Show
+data ECType = ExpPrm | ExpCh2 | NmdCrv | CRaw Word8 deriving Show
 
-instance B.Bytable EcCurveType where
-	encode ExplicitPrime = BS.pack [1]
-	encode ExplicitChar2 = BS.pack [2]
-	encode NamedCurve = BS.pack [3]
-	encode (ECTRaw w) = BS.pack [w]
+instance B.Bytable ECType where
+	encode ExpPrm = BS.pack [1]
+	encode ExpCh2 = BS.pack [2]
+	encode NmdCrv = BS.pack [3]
+	encode (CRaw w) = BS.pack [w]
 	decode = B.evalBytableM B.parse
 
-instance B.Parsable EcCurveType where
-	parse = do
-		ct <- B.head
-		return $ case ct of
-			1 -> ExplicitPrime
-			2 -> ExplicitChar2
-			3 -> NamedCurve
-			w -> ECTRaw w
+instance B.Parsable ECType where
+	parse = (\t -> case t of 1 -> ExpPrm; 2 -> ExpCh2; 3 -> NmdCrv; _ -> CRaw t)
+		<$> B.head
 
 instance B.Bytable ECC.CurveName where
 	encode ECC.SEC_t163k1 = B.encode (1 :: Word16)
@@ -139,10 +128,21 @@ instance B.Bytable ECC.CurveName where
 	encode ECC.SEC_t193r2 = B.encode (5 :: Word16)
 	encode ECC.SEC_t233k1 = B.encode (6 :: Word16)
 	encode ECC.SEC_t233r1 = B.encode (7 :: Word16)
+	encode ECC.SEC_t239k1 = B.encode (8 :: Word16)
+	encode ECC.SEC_t283k1 = B.encode (9 :: Word16)
+	encode ECC.SEC_t283r1 = B.encode (10 :: Word16)
 	encode ECC.SEC_t409k1 = B.encode (11 :: Word16)
 	encode ECC.SEC_t409r1 = B.encode (12 :: Word16)
 	encode ECC.SEC_t571k1 = B.encode (13 :: Word16)
 	encode ECC.SEC_t571r1 = B.encode (14 :: Word16)
+	encode ECC.SEC_p160k1 = B.encode (15 :: Word16)
+	encode ECC.SEC_p160r1 = B.encode (16 :: Word16)
+	encode ECC.SEC_p160r2 = B.encode (17 :: Word16)
+	encode ECC.SEC_p192k1 = B.encode (18 :: Word16)
+	encode ECC.SEC_p192r1 = B.encode (19 :: Word16)
+	encode ECC.SEC_p224k1 = B.encode (20 :: Word16)
+	encode ECC.SEC_p224r1 = B.encode (21 :: Word16)
+	encode ECC.SEC_p256k1 = B.encode (22 :: Word16)
 	encode ECC.SEC_p256r1 = B.encode (23 :: Word16)
 	encode ECC.SEC_p384r1 = B.encode (24 :: Word16)
 	encode ECC.SEC_p521r1 = B.encode (25 :: Word16)
@@ -156,66 +156,68 @@ instance B.Bytable ECC.CurveName where
 			(5 :: Word16) -> Right ECC.SEC_t193r2
 			(6 :: Word16) -> Right ECC.SEC_t233k1
 			(7 :: Word16) -> Right ECC.SEC_t233r1
+			(8 :: Word16) -> Right ECC.SEC_t239k1
+			(9 :: Word16) -> Right ECC.SEC_t283k1
+			(10 :: Word16) -> Right ECC.SEC_t283r1
 			(11 :: Word16) -> Right ECC.SEC_t409k1
 			(12 :: Word16) -> Right ECC.SEC_t409r1
 			(13 :: Word16) -> Right ECC.SEC_t571k1
 			(14 :: Word16) -> Right ECC.SEC_t571r1
+			(15 :: Word16) -> Right ECC.SEC_p160k1
+			(16 :: Word16) -> Right ECC.SEC_p160r1
+			(17 :: Word16) -> Right ECC.SEC_p160r2
+			(18 :: Word16) -> Right ECC.SEC_p192k1
+			(19 :: Word16) -> Right ECC.SEC_p192r1
+			(20 :: Word16) -> Right ECC.SEC_p224k1
+			(21 :: Word16) -> Right ECC.SEC_p224r1
+			(22 :: Word16) -> Right ECC.SEC_p256k1
 			(23 :: Word16) -> Right ECC.SEC_p256r1
 			(24 :: Word16) -> Right ECC.SEC_p384r1
 			(25 :: Word16) -> Right ECC.SEC_p521r1
-			n -> Left $ "Extension: CurveName.decode: unknown curve: " ++
+			n -> Left $ modNm ++ ": CurveName.decode: unknown crv: " ++
 				show n
-		_ -> Left "Extension: CurveName.decode: bad format"
+		_ -> Left $ modNm ++ ": CurveName.decode: bad format"
 
-instance B.Parsable ECC.CurveName where
-	parse = B.take 2
+instance B.Parsable ECC.CurveName where parse = B.take 2
 
 instance B.Bytable ECC.Curve where
 	encode c
 		| c == ECC.getCurveByName ECC.SEC_p256r1 =
-			B.encode NamedCurve `BS.append` B.encode ECC.SEC_p256r1
-		| otherwise = error "TlsServer.encodeC: not implemented"
+			B.encode NmdCrv `BS.append` B.encode ECC.SEC_p256r1
+		| c == ECC.getCurveByName ECC.SEC_p384r1 =
+			B.encode NmdCrv `BS.append` B.encode ECC.SEC_p384r1
+		| c == ECC.getCurveByName ECC.SEC_p521r1 =
+			B.encode NmdCrv `BS.append` B.encode ECC.SEC_p521r1
+		| otherwise = error $ modNm ++ ": ECC.Curve.encode: not implemented"
 	decode = B.evalBytableM B.parse
 
 instance B.Parsable ECC.Curve where
-	parse = ECC.getCurveByName <$> do
-		NamedCurve <- B.parse
-		B.parse
+	parse = (ECC.getCurveByName <$>) $ B.parse >>= \t -> case t of
+		NmdCrv -> B.parse
+		_ -> error $ modNm ++ ": ECC.Curve.parse: not implemented"
 
-data EcPointFormat = EPFUncompressed | EPFRaw Word8 deriving (Show, Eq)
+data EPFrmt = EPFUncomp | EPFRaw Word8 deriving (Show, Eq)
 
-instance B.Bytable EcPointFormat where
-	encode EPFUncompressed = BS.pack [0]
+instance B.Bytable EPFrmt where
+	encode EPFUncomp = BS.pack [0]
 	encode (EPFRaw f) = BS.pack [f]
 	decode bs = case BS.unpack bs of
-		[f] -> Right $ case f of 0 -> EPFUncompressed; _ -> EPFRaw f
-		_ -> Left "Extension: Bytable.decode"
+		[f] -> Right $ case f of 0 -> EPFUncomp; _ -> EPFRaw f
+		_ -> Left $ modNm ++ ": Bytable.decode"
 
 instance B.Bytable ECC.Point where
 	encode (ECC.Point x y) = B.addLen w8 $
-		4 `BS.cons` padd 32 0 (B.encode x) `BS.append`
-			padd 32 0 (B.encode y)
-	encode ECC.PointO = error "Extension: EC.Point.encode"
+		4 `BS.cons` pad 32 0 (B.encode x) `BS.append` pad 32 0 (B.encode y)
+		where pad n w s = BS.replicate (n - BS.length s) w `BS.append` s
+	encode ECC.PointO = error $ modNm ++ ": EC.Point.encode"
 	decode = B.evalBytableM B.parse
 
-padd :: Int -> Word8 -> BS.ByteString -> BS.ByteString
-padd n w s = BS.replicate (n - BS.length s) w `BS.append` s
-
 instance B.Parsable ECC.Point where
-	parse = do
-		bs <- B.take =<< B.take 1
-		case BS.uncons bs of
-			Just (4, rest) -> return $ let (x, y) = BS.splitAt 32 rest in
-				ECC.Point
-					(either error id $ B.decode x)
-					(either error id $ B.decode y)
-			_ -> fail "Extension: ECC.Point.parse"
-
-w8 :: Word8; w8 = undefined
-w16 :: Word16; w16 = undefined
-
-cmap :: (a -> BS.ByteString) -> [a] -> BS.ByteString
-cmap = (BS.concat .) . map
+	parse = (B.take =<< B.take 1) >>= \bs -> case BS.uncons bs of
+		Just (4, xy) -> return $ let (x, y) = BS.splitAt 32 xy in ECC.Point
+			(either error id $ B.decode x)
+			(either error id $ B.decode y)
+		_ -> fail $ modNm ++ ": ECC.Point.parse"
 
 isRnInfo :: Extension -> Bool
 isRnInfo (ERnInfo _) = True
@@ -223,3 +225,6 @@ isRnInfo _ = False
 
 emptyRnInfo :: Extension
 emptyRnInfo = ERnInfo ""
+
+w8 :: Word8; w8 = undefined
+w16 :: Word16; w16 = undefined
