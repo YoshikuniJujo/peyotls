b6308bddec77ad14c8efe4d12016b1db8cd3f41b
Yoshikuni Jujo <PAF01143@nifty.ne.jp>
2014-08-22 11:44:10 +0900

    #110 asyncServer: make independent function open

--- debug/asyncServer.hs
+++ debug/asyncServer.hs
@@ -1,17 +1,21 @@
-{-# LANGUAGE OverloadedStrings, PackageImports #-}
+{-# LANGUAGE OverloadedStrings, FlexibleContexts,
+	PackageImports #-}
 
 import Control.Applicative
 import Control.Monad
 import "monads-tf" Control.Monad.State
 import Control.Monad.Base
+import Control.Monad.Trans.Control
 import Control.Concurrent
 import Control.Concurrent.STM
 import Data.Word
 import Data.HandleLike
+import Data.X509
+import Data.X509.CertificateStore
 import System.Environment
-import System.IO
 import Network
-import Network.PeyoTLS.Server
+import Network.PeyoTLS.Server hiding (open)
+import qualified Network.PeyoTLS.Server as S
 import Network.PeyoTLS.ReadFile
 import Network.PeyoTLS.Run.Crypto
 import "crypto-random" Crypto.Random
@@ -31,44 +35,8 @@ main = do
 		(h, _, _) <- liftIO $ accept soc
 		g <- StateT $ return . cprgFork
 		liftIO . forkIO $ do
-
-			inc <- atomically newTChan
-			otc <- atomically newTChan
-			((k, n), g') <- (`run'` g) $ do
-				p <- open h ["TLS_RSA_WITH_AES_128_CBC_SHA"]
-					[(k, c)] Nothing
-				hlFlush p
-			let	rk = kRKey k
-				rmk = kRMKey k
-				wk = kWKey k
-				wmk = kWMKey k
-			forkIO . (`evalStateT` 1) . forever $ do
-				sn <- get
-				modify succ
-				liftBase $ putStrLn $ "sn = " ++ show sn
-				pre <- lift $ hlGet h 3
-				when (BSC.null pre) $
-					liftBase (hClose h) >> error "bad"
-				liftBase $ print pre
-				Right n <- B.decode <$> lift (hlGet h 2)
-				liftBase $ print n
-				renc <- lift $ hlGet h n
-				let Right rpln = decrypt sha1 rk rmk sn pre renc
-				liftBase $ do
-					BS.putStr rpln
-					atomically $ writeTChan inc rpln
-
-			forkIO . (`evalStateT` (1, g')) . forever $ do
-				(sn, g0) <- get
-				wpln <- liftBase . atomically $ readTChan otc
-				let	(wenc, g1) =
-						encrypt sha1 wk wmk sn "\ETB\ETX\ETX" wpln g'
-				put (succ sn, g1)
-				lift $ hlPut h "\ETB\ETX\ETX"
-				lift . hlPut h
-					. (B.encode :: Word16 -> BSC.ByteString)
-					. fromIntegral $ BSC.length wenc
-				lift $ hlPut h wenc
+			(_n, (inc, otc)) <- open h ["TLS_RSA_WITH_AES_128_CBC_SHA"]
+				[(k, c)] Nothing g
 
 			let	wpln = BS.concat [
 					"HTTP/1.1 200 OK\r\n",
@@ -81,3 +49,49 @@ main = do
 			atomically $ writeTChan otc wpln
 			BS.putStr =<< atomically (readTChan inc)
 			atomically $ writeTChan otc wpln
+
+open :: (CPRG g, ValidateHandle h, MonadBaseControl IO (HandleMonad h)) =>
+	h -> [CipherSuite] -> [(CertSecretKey, CertificateChain)] ->
+	Maybe CertificateStore -> g ->
+	HandleMonad h ([String], (TChan BSC.ByteString, TChan BSC.ByteString))
+open h cs kcs ca g = do
+			inc <- liftBase $ atomically newTChan
+			otc <- liftBase $ atomically newTChan
+			((k, ns), g') <- (`run'` g) $ do
+				p <- S.open h cs kcs ca
+				hlFlush p
+			let	rk = kRKey k
+				rmk = kRMKey k
+				wk = kWKey k
+				wmk = kWMKey k
+			_ <- liftBaseDiscard forkIO
+				. (`evalStateT` 1) . forever $ do
+				sn <- get
+				modify succ
+				liftBase $ putStrLn $ "sn = " ++ show sn
+				pre <- lift $ hlGet h 3
+				when (BSC.null pre) $
+					lift (hlClose h) >> error "bad"
+				liftBase $ print pre
+				Right n <- B.decode <$> lift (hlGet h 2)
+				liftBase $ print n
+				renc <- lift $ hlGet h n
+				let Right rpln = decrypt sha1 rk rmk sn pre renc
+				liftBase $ do
+					BS.putStr rpln
+					atomically $ writeTChan inc rpln
+
+			_ <- liftBaseDiscard forkIO
+				. (`evalStateT` (1, g')) . forever $ do
+				(sn, g0) <- get
+				wpln <- liftBase . atomically $ readTChan otc
+				let	(wenc, g1) =
+						encrypt sha1 wk wmk sn "\ETB\ETX\ETX" wpln g0
+				put (succ sn, g1)
+				lift $ hlPut h "\ETB\ETX\ETX"
+				lift . hlPut h
+					. (B.encode :: Word16 -> BSC.ByteString)
+					. fromIntegral $ BSC.length wenc
+				lift $ hlPut h wenc
+
+			return (ns, (inc, otc))
